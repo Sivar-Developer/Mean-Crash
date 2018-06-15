c2877f2b7db8b096507861dc586eceb1d089304a
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-06-15 04:19:35 +0300

    Material Design Integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule , MatCardModule , MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
